d20ab9c0b571d362a17c07bfeb26a96b1c8a47f7
Tristan Aumentado-Armstrong <t.aumentado@live.ca>
2015-09-27 02:59:07 -0400

    Fixing merge

---
+++ lostStolenCardCheck.py
@@ -0,0 +1,10 @@
+#sys.path.insert(0, mastercardApiPythonPath)
+#		p = 'mc_python_api.mastercard-api-python.Common'
+#		from mc_python_api.mastercard-api-python.common.Environment import environment
+#		from services.lost_stolen import loststolenservice
+import sys
+sys.path.append('C:/Users/Tristan/MasterCardApp/mastercard-api-python/Services/lost_stolen')
+sys.path.append('C:/Users/Tristan/MasterCardApp/mastercard-api-python/Common/')
+
+from common import environment
+
