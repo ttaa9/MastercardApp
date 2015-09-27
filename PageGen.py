5f614bbb0e89c4c4ae8d48c92160a6697bf182ab
Tristan Aumentado-Armstrong <t.aumentado@live.ca>
2015-09-26 23:09:49 -0400

    Done with cgi, please god

--- PageGen.py
+++ PageGen.py
@@ -1,4 +1,5 @@
 #!C:\Users\Tristan\Anaconda\python.exe -u
+# -*- coding: UTF-8 -*-
 
 from Person import PersonInfo
 from Person import *
@@ -24,9 +25,6 @@ def isValidUP(username,password):
 #
 
 
-
-
-
 #####################
 # CGI Generate Page #
 #####################
@@ -41,8 +39,135 @@ if not isValidUP(val1,val2):
 
 ### Authentication Worked ###
 else:
-	print "Content-type: text/html"
-	print
-	print "<title>Test CGI</title>"
-	print """ Valid U+P: %s %s
-	</body></html>""" % (val1, val2)
+	p = PersonInfo(val1)
+	print '''
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Profile</title>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" href="css/masterStyles.css" type="text/css">
+	<script src="js/masterscript.js"></script>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	</head>
+	<body>
+	<nav class="navbar navbar-default navbar-inverse" style="border-radius:0px;">
+	  <div class="container-fluid"> 
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+	      <div class="proj-name"><img src="img/mc.png" height="25px"/> Spend to Save</div> 
+	    </div>
+	    
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li class="active"><a href="#">Home<span class="sr-only">(current)</span></a> </li>
+	        <li><a href="donations.html">Donation Manager</a> </li>
+	        <li><a href="charities.html">Charity Settings</a> </li>
+	      </ul>
+	    </div>
+	    <!-- /.navbar-collapse --> 
+	  </div>
+	  <!-- /.container-fluid --> 
+	</nav>
+	<div id="user-info" class="container-fluid">
+	  <div class="row">
+	    <div class="gen-info">
+	    	<div class="user-pic">
+	       		<img src="img/gir.jpg" height="150px" id="user-pic"/>
+	        </div>
+	        <div class="user-info"> '''+str(p.firstName)+" "+str(p.lastName)+'''
+	        	<br/>''' + str(p.level) + ''' <br/><br/>
+	            Donations to next level:<br/>
+	            <div class="progress" style="width:90%;margin-left:5%">
+	            	<div class="progress-bar progress-bar-striped" 
+	                	role="progressbar" aria-valuenow="50"
+	                    aria-valuemin="0" aria-valuemax="100" style="width:'''+str(int(float(p.getPercentageToNextLevel())))+'''%">'''+p.getPercentageToNextLevel()+'''% to '''+p.getNameOfNextLevel()+'''
+	  				</div>
+	  			</div>
+	            <br/>'''+str(p.username)+'''<br/><br/>'''+"Card ending in: "+str(p.cardNum)[-4:]+'''
+	        </div>
+	    </div>
+	  </div>
+	</div>
+
+	<div id="donation-info" class="content">
+	  	<div class="row">
+	  		<div class="title">
+	    		Welcome '''+('Mr. ' if p.gender=='M' else 'Ms. ')+p.lastName+'''</div>
+	    </div>
+	    <br/>
+	  	<div id="total" class="row">
+	  		<div class="col-md-4 donation-text">
+	        	Total Donations To Date:
+	        </div>
+	        <div class="col-md-3 donation-text">
+	        	<div class="">
+	    			$'''+str(p.getTotalDonations())+'''CAD
+	    			
+	  			</div>
+	        </div>
+	  	</div>
+	    <div id="charities-helped" class="row">
+	  		<div class="col-md-5 donation-text">
+	        	Charities Donated to:      
+	        </div>
+	  	</div>
+	    <br/>
+	    <div class="table-responsive" style="width:80%;margin-left:10%;">
+	    	<table class="table table-striped table-hover">
+	    <thead>
+	      <tr>
+	        <th>Charity</th>
+	        <th>Date Last Donated</th>
+	        <th>Last Donation</th>
+	        <th>Lifetime Total</th>
+	      </tr>
+	    </thead>
+
+	    <tbody> '''+p.generateDonationTable()+'''</tbody>
+
+	  </table>
+
+		<br>
+		<img src="img/facebook.png" height="25px"/>
+		Link your Facebook
+
+
+		<br>
+		<img src="img/Twitter-icon.png.png" height="25px"/>
+		Link your Twitter
+
+	    </div>
+	  </div>
+	<br/>
+
+	<hr>
+	<footer class="text-center">
+	  <div class="container" style="padding-left:350px;">
+	    <div class="row">
+	      <div class="col-xs-12" style="margin-bottom:10px;">
+	        <p>Copyright © 2015 BXT SENC. All rights reserved.</p>
+	      </div>
+	    </div>
+	  </div>
+	</footer>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+	<script src="js/jquery-1.11.2.min.js"></script> 
+	<!-- Include all compiled plugins (below), or include individual files as needed --> 
+	<script src="js/bootstrap.min.js"></script>
+	</body>
+	</html>
+	'''
--- Person.py
+++ Person.py
@@ -9,14 +9,14 @@ accountsFile = "accounts.csv"
 mastercardApiPythonPath = "mc_python_api/mastercard-api-python"
 
 # Global Variables and Methods
-levels = collections.OrderedDict([(0,"Kind Person"), (20,"Good Samaritan"), 
-	(100,"Beginner Philanthropist"), (500,"Advanced Samaritan"), 
-	(1000,"Angel")])
+levels = collections.OrderedDict([(0,"Giver"), (20,"Good Samaritan"), 
+	(100,"Beginner Philanthropist"), (500,"Advanced Philanthropist"), 
+	(1000,"Angel"), (1000000,"Saint")])
 
 def getLevel(amount):
 	for i,key in enumerate(levels.keys()):
 		if amount < key: return levels.values()[i-1]
-
+	return "DEFAULT"
 # Account class
 class PersonInfo: #(object):
 	
@@ -28,17 +28,18 @@ class PersonInfo: #(object):
 		if os.path.isfile(fs):
 			# Read in settings
 			i=0
-			self.username = f[i]
 			f = [p.strip() for p in open(fs,"r").readlines()]
+			self.username = f[i]
 			self.cardNum = f[i+1]
 			self.totalDonations = f[i+2]
-			self.level = getLevel(self.totalDonations)
 			self.firstName = f[i+3]
 			self.lastName = f[i+4]
+			self.gender = f[i+5]
 			# Read in transaction history (line: charityName,amount)
 			f2 = open(fth,"r").readlines()
 			temp = [ k.strip().split(",") for k in f2]
-			self.transactionHistory = [ [t[0], float(t[1])] for t in temp ]
+			self.transactionHistory = [ [t[0],t[1],float(t[2])] for t in temp ]
+			self.level = getLevel(self.getTotalDonations())
 		else:
 			print(fs)
 			assert False, "New account Capability Turned Off"
@@ -48,8 +49,9 @@ class PersonInfo: #(object):
 			self.totalDonations = 0
 			self.firstName = ""
 			self.lastName = ""
-			self.level = getLevel(self.totalDonations)
 			self.transactionHistory = {}
+			self.gender = "Not Set"
+			self.level = getLevel(self.getTotalDonations())
 			# Write a new file
 			self.write()
 
@@ -60,7 +62,7 @@ class PersonInfo: #(object):
 #		p = 'mc_python_api.mastercard-api-python.Common'
 #		from mc_python_api.mastercard-api-python.common.Environment import environment
 #		from services.lost_stolen import loststolenservice
-		print("hi")
+	#	print("hi")
 		# Check validity
 
 		# Set card Number
@@ -68,10 +70,10 @@ class PersonInfo: #(object):
 
 	def toSettingsString(self):
 		return "\n".join([self.username, str(self.cardNum),
-			str(self.totalDonations), self.firstName, self.lastName])
+			str(self.getTotalDonations()), self.firstName, self.lastName, self.gender])
 
 	def makeTransactionHistoryString(self):
-		ss = [ str(k[0]) + "," + str(k[1]) for k in self.transactionHistory ]
+		ss = [ str(k[0]) + "," + str(k[1]) + "," + str(k[2]) for k in self.transactionHistory ]
 		return "\n".join(ss)
 
 	# Writes "person" object to files
@@ -85,6 +87,82 @@ class PersonInfo: #(object):
 		thFile = open(fth,"w")
 		thFile.write(self.makeTransactionHistoryString())
 		thFile.close()
+
+	def getTotalDonations(self):
+		return sum( [k[2] for k in self.transactionHistory] )
+
+	@staticmethod
+	def firstDateStringIsGreater(s1,s2):
+		m1,d1,y1 = s1.split("/")
+		m2,d2,y2 = s2.split("/")
+		if y1>y2: return True
+		if m1>m2: return True
+		if d1>d2: return True
+		return False
+
+	def getPercentageToNextLevel(self):
+		total = self.getTotalDonations()
+		for i,key in enumerate(levels.keys()):
+			if total < key: 
+				nextLevAmount = levels.keys()[i]
+				k = float(total)/float(nextLevAmount)*100
+				return '{00:.{1}f}'.format(k,1)
+			#	return levels.values()[i-1]
+		return "DEFAULT"
+
+	def getNameOfNextLevel(self):
+		for i,key in enumerate(levels.keys()):
+			if self.getTotalDonations() < key: return levels.values()[i]
+		return "DEFAULT"
+
+	def generateDonationTable(self):
+		chars = []
+		lastDate = []
+		lastAmount = []
+		lifetimeTotals = []
+		for e in self.transactionHistory:
+			if e[0] in chars:
+				ind = chars.index(e[0])
+				if PersonInfo.firstDateStringIsGreater(e[1],lastDate[ind]): # current no greater
+					lastDate[ind] = e[1]
+					lastAmount[ind] = e[2]
+				lifetimeTotals[ind] += e[2] 
+			else:
+				chars.append(e[0])
+				lastDate.append(e[1])
+				lastAmount.append(e[2])
+				lifetimeTotals.append(e[2])
+		s = ""
+		for i,c in enumerate(chars):
+			s += "<tr>"
+			s += "<td>"+str(c)+"</td>"
+			s += "<td>"+str(lastDate[i])+"</td>"
+			s += "<td>$"+str(lastAmount[i])+"</td>"
+			s += "<td>$"+str(lifetimeTotals[i])+"</td>"
+			s += "</tr>"
+		print(s)
+		return s
+
+		'''
+			      <tr>
+	        <td>MSF</td>
+	        <td>02/02/2015</td>
+	        <td>$4.53</td>
+	        <td>$24.53</td>
+	      </tr>
+	      <tr>
+	        <td>Japanese Tsunami Relief Fund</td>
+	        <td>02/09/2013</td>
+	        <td>$1.22</td>
+	        <td>$132.98</td>
+	      </tr>
+	      <tr>
+	        <td>UNICEF Syrian Crisis Fund</td>
+	        <td>11/09/2012</td>
+	        <td>$9.47</td>
+	        <td>$9.47</td>
+	      </tr>
+		'''
 
 
 
